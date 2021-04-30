0a54ebd4b424f0b964ccff72901ef87f34b86ef1
Anubhav2907 <76243969+Anubhav2907@users.noreply.github.com>
2021-04-30 18:19:35 +0530

    fancy cluster map on home

--- seeds/index.js
+++ seeds/index.js
@@ -12,7 +12,7 @@ db.once("open", function () {
 const sample = array => array[Math.floor(Math.random() * array.length)]
 const seedDB = async function () {
     await Campground.deleteMany({});
-    for (let i = 0; i < 50; i++) {
+    for (let i = 0; i < 200; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
@@ -20,7 +20,10 @@ const seedDB = async function () {
             title: `${sample(descriptors)} ${sample(places)}`,
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             geometry: {
-                coordinates: [78.19306, 26.215],
+                coordinates: [
+                    cities[random1000].longitude,
+                    cities[random1000].latitude,
+                ],
                 type: "Point"
             },
             images: {
